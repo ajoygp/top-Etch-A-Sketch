e737f176cc0ac5d100bfa46c93c1bc9562b72d6b
ajoygp <despajoy@gmail.com>
2022-08-19 21:39:14 +0530

    Modify function to create n divs within n div containers

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 let container=document.querySelector(".container");
 
-function createGrids(len=16){
-    for(let i=0;i<len;i++){
-        let grid = document.createElement("div");
-        grid.className="grid";
-        container.appendChild(grid);
+function createGrids(len=16){        
+    for(let row=0;row<len;row++){
+        let gridRow = document.createElement("div");
+        gridRow.className="gridRow";
+        for(let i=0;i<len;i++){
+            let grid = document.createElement("div");
+            grid.className="grid";
+            gridRow.appendChild(grid);
+        }
+        container.appendChild(gridRow);
     }
 }
 
