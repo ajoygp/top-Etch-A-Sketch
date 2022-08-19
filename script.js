f0d4892707eb74bfdfe31e0dbb213c68edd7b47c
ajoygp <despajoy@gmail.com>
2022-08-20 01:39:06 +0530

    Create random color on hover functionality

--- script.js
+++ script.js
@@ -1,16 +1,45 @@
 let container=document.querySelector(".container");
+let sizebutton=document.querySelector(".size-button");
 
-function createGrids(len=16){        
+function randomColor(){
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function createGrids(len=16){   
+    len = len>50? 50:len;
+    len = len<10? 10:len; 
+     
     for(let row=0;row<len;row++){
         let gridRow = document.createElement("div");
         gridRow.className="gridRow";
         for(let i=0;i<len;i++){
             let grid = document.createElement("div");
             grid.className="grid";
+            grid.addEventListener("mouseover",()=>{
+                grid.style.backgroundColor=randomColor();
+            })
             gridRow.appendChild(grid);
         }
         container.appendChild(gridRow);
     }
 }
 
-createGrids()
+createGrids();
+
+function getsize(){
+    let s =window.prompt("Enter grid size (min 10, max 50)")
+    if(!Number(s)){
+        window.alert("Enter a valid number");
+        s =window.prompt("Enter grid size (min 10, max 50)")
+    }  
+    if(s){
+        container.innerHTML="";
+        createGrids(s);
+    }
+}
+sizebutton.addEventListener("click",getsize);
